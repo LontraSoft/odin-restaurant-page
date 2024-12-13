57925e6a40b66a9fccb63a80686e6798a0068ff7
Robin_Lontra <189180083+LontraSoft@users.noreply.github.com>
2024-12-13 14:51:01 -0600

    Add content switching functions to button click events

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
 import "./index.css"
 import {initialLoad} from "./initial-load.js";
 
+const homeButton = document.querySelector("#home-button");
+const menuButton = document.querySelector("#menu-button");
+const aboutButton = document.querySelector("#about-button");
+
+function switchContentHome() {
+    console.log("Switching to home content");
+}
+
+function switchContentMenu() {
+    console.log("Switching to menu content");
+}
+
+function switchContentAbout() {
+    console.log("Switching to about content");
+}
+
+homeButton.addEventListener("click", switchContentHome);
+menuButton.addEventListener("click", switchContentMenu);
+aboutButton.addEventListener("click", switchContentAbout);
+
 initialLoad();
+
