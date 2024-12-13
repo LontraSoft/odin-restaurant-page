783ad7ffda18a0043c5da1531be95aa41b7f1e58
Robin_Lontra <189180083+LontraSoft@users.noreply.github.com>
2024-12-13 00:57:02 -0600

    Add elements to enable proper styling

--- src/initial-load.js
+++ src/initial-load.js
@@ -5,18 +5,25 @@ function initialLoad() {
     let contentContainer = document.querySelector("#content");
     let heroDiv = document.createElement("div");
     let heroImg = document.createElement("img");
+    let heroTextContainer = document.createElement("div");
     let heroHeader = document.createElement("h1");
     let heroText = document.createElement("p");
 
     heroDiv.className = "hero";
-    heroImg.src = "hero";
+    heroImg.id = "hero-image";
+    heroImg.src = heroImage;
+    heroTextContainer.className = "hero-text-container";
     heroHeader.lang = "fr";
+    heroHeader.id = "hero-header";
     heroHeader.textContent = HERO_HEADER_TEXT;
+    heroText.id = "hero-text";
     heroText.textContent = HERO_TEXT;
 
+    heroTextContainer.appendChild(heroHeader);
+    heroTextContainer.appendChild(heroText);
+
     heroDiv.appendChild(heroImg);
-    heroDiv.appendChild(heroHeader);
-    heroDiv.appendChild(heroText);
+    heroDiv.appendChild(heroTextContainer);
 
     contentContainer.appendChild(heroDiv);
 }
