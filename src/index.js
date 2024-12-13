3949224adae70181f8b2a1b704a2d510d388a3d1
Robin_Lontra <189180083+LontraSoft@users.noreply.github.com>
2024-12-13 15:28:52 -0600

    Add clearing of content on hitting nav butttons

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
 import "./index.css"
 import {initialLoad, homeContent} from "./initial-load.js";
 
+const contentContainer = document.querySelector("#content");
 const homeButton = document.querySelector("#home-button");
 const menuButton = document.querySelector("#menu-button");
 const aboutButton = document.querySelector("#about-button");
 
+const EMPTY_HTML = "";
+
+function clearContent() {
+    contentContainer.innerHTML = EMPTY_HTML;
+}
+
 function switchContentHome() {
     console.log("Switching to home content");
+    clearContent();
 }
 
 function switchContentMenu() {
     console.log("Switching to menu content");
+    clearContent();
 }
 
 function switchContentAbout() {
     console.log("Switching to about content");
+    clearContent();
 }
 
 homeButton.addEventListener("click", switchContentHome);
